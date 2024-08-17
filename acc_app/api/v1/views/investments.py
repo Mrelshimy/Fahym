3e7580dd68dd0891d6e286c5e4aeb433d3a147c8
Mrelshimy <mraafat.elsayed@gmail.com>
2024-08-17 19:21:31 +0300

    Adding investments view

--- acc_app/api/v1/views/investments.py
+++ acc_app/api/v1/views/investments.py
@@ -8,32 +8,38 @@ from uuid import uuid4
 @views_bp.route('/investments', methods=['GET'], strict_slashes=False)
 def get_investments():
     with app.app_context():
-        items = db.session.query(Investment).order_by(Investment.created_at).all()
+        items = db.session.query(Investment).\
+            order_by(Investment.created_at).all()
         data = [item.to_dict() for item in items]
         return jsonify(data), 200
 
 
-@views_bp.route('/users/<user_id>/initial_investment', methods=['GET'], strict_slashes=False)
+@views_bp.route('/users/<user_id>/initial_investment',
+                methods=['GET'], strict_slashes=False)
 def get_initial_investment(user_id):
     with app.app_context():
         user = db.session.query(User).filter_by(id=user_id).first()
         if user:
-            items = db.session.query(Investment).filter_by(user_id=user_id).order_by(Investment.created_at).all()
+            items = db.session.query(Investment).filter_by(user_id=user_id)\
+                .order_by(Investment.created_at).all()
             data = [item.to_dict() for item in items]
             return jsonify(data[-1]), 200
         else:
             abort(404, 'User not found')
 
 
-@views_bp.route('users/<user_id>/investments', methods=['GET'], strict_slashes=False)
+@views_bp.route('users/<user_id>/investments',
+                methods=['GET'], strict_slashes=False)
 def get_user_investments(user_id):
     with app.app_context():
-        items = db.session.query(Investment).filter_by(user_id=user_id).order_by(Investment.created_at).all()
+        items = db.session.query(Investment).filter_by(user_id=user_id)\
+            .order_by(Investment.created_at).all()
         data = [item.to_dict() for item in items]
         return jsonify(data), 200
 
 
-@views_bp.route('users/<user_id>/investments', methods=['POST'], strict_slashes=False)
+@views_bp.route('users/<user_id>/investments',
+                methods=['POST'], strict_slashes=False)
 def add_investments(user_id):
     with app.app_context():
         if request.is_json:
@@ -53,7 +59,7 @@ def add_investments(user_id):
                 investment = Investment(**data)
                 db.session.add(investment)
                 db.session.commit()
-                return jsonify({"message": "Investment added successfully"}), 200
+                return jsonify({"message": "Investment added"}), 200
             else:
                 abort(404, 'User not found')
         else:
