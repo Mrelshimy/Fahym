0747836e709c2aee2f28f3ea129309986154257c
Mrelshimy <mraafat.elsayed@gmail.com>
2024-08-17 19:19:03 +0300

    Adding customers  view

--- acc_app/api/v1/views/customers.py
+++ acc_app/api/v1/views/customers.py
@@ -9,12 +9,14 @@ from uuid import uuid4
 @views_bp.route('/customers', methods=['GET'], strict_slashes=False)
 def get_customers():
     with app.app_context():
-        customers = db.session.query(Customer).order_by(desc(Customer.created_at)).all()
+        customers = db.session.query(Customer)\
+            .order_by(desc(Customer.created_at)).all()
         data = [customer.to_dict() for customer in customers]
         return jsonify(data), 200
 
 
-@views_bp.route('/customers/<customer_id>', methods=['GET'], strict_slashes=False)
+@views_bp.route('/customers/<customer_id>',
+                methods=['GET'], strict_slashes=False)
 def get_customer(customer_id):
     with app.app_context():
         customer = db.session.query(Customer).get(customer_id)
@@ -23,15 +25,18 @@ def get_customer(customer_id):
         return jsonify(customer.to_dict()), 200
 
 
-@views_bp.route('/users/<user_id>/customers', methods=['GET'], strict_slashes=False)
+@views_bp.route('/users/<user_id>/customers',
+                methods=['GET'], strict_slashes=False)
 def get_user_customers(user_id):
     with app.app_context():
-        customers = db.session.query(Customer).filter_by(user_id=user_id).order_by(desc(Customer.created_at)).all()
+        customers = db.session.query(Customer).filter_by(user_id=user_id)\
+            .order_by(desc(Customer.created_at)).all()
         data = [customer.to_dict() for customer in customers]
         return jsonify(data), 200
 
 
-@views_bp.route('/users/<user_id>/customers', methods=['POST'], strict_slashes=False)
+@views_bp.route('/users/<user_id>/customers',
+                methods=['POST'], strict_slashes=False)
 def post_customer(user_id):
     with app.app_context():
         if request.is_json:
@@ -39,7 +44,8 @@ def post_customer(user_id):
             not_needed = ['id', 'created_at', 'updated_at']
             for value in not_needed:
                 if value in data:
-                    abort(400, 'id, created_at and updated_at are not needed')
+                    abort(400,
+                          'id, created_at and updated_at are not needed')
             required = ['code', 'name']
             for value in required:
                 if value not in data:
@@ -54,7 +60,8 @@ def post_customer(user_id):
             abort(400, 'Not a JSON')
 
 
-@views_bp.route('/customers/<customer_id>', methods=['DELETE'], strict_slashes=False)
+@views_bp.route('/customers/<customer_id>',
+                methods=['DELETE'], strict_slashes=False)
 def delete_customer(customer_id):
     with app.app_context():
         customer = db.session.query(Customer).get(customer_id)
@@ -65,7 +72,8 @@ def delete_customer(customer_id):
         return jsonify({"message": "customer deleted successfully"}), 200
 
 
-@views_bp.route('/customers/<customer_id>', methods=['PUT'], strict_slashes=False)
+@views_bp.route('/customers/<customer_id>',
+                methods=['PUT'], strict_slashes=False)
 def update_customer(customer_id):
     with app.app_context():
         customer = db.session.query(Customer).get(customer_id)
@@ -73,11 +81,9 @@ def update_customer(customer_id):
             abort(404)
         if request.is_json:
             data = request.get_json()
+            not_required = ['id', 'created_at', 'updated_at', 'user_id']
             for key, value in data.items():
-                if key == "created_at" or\
-                    key == "updated_at" or\
-                        key == 'user_id' or\
-                            key == "id":
+                if key in not_required:
                     continue
                 else:
                     setattr(customer, key, value)
