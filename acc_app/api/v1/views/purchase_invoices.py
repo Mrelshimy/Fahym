f182652b93e7647ff96dffb19046fa1386290606
Mrelshimy <mraafat.elsayed@gmail.com>
2024-08-17 19:29:10 +0300

    Adding comments

--- acc_app/api/v1/views/purchase_invoices.py
+++ acc_app/api/v1/views/purchase_invoices.py
@@ -6,15 +6,18 @@ from acc_app.models.models import Purchase_invoice, Supplier, Item, purchase_ite
 from uuid import uuid4
 from datetime import datetime
 
-@views_bp.route('/purchase_invoices', methods=['GET'], strict_slashes=False)
+@views_bp.route('/purchase_invoices',
+                methods=['GET'], strict_slashes=False)
 def get_purchase_invoices():
     with app.app_context():
-        invoices = db.session.query(Purchase_invoice).order_by(desc(Purchase_invoice.created_at)).all()
+        invoices = db.session.query(Purchase_invoice)\
+            .order_by(desc(Purchase_invoice.created_at)).all()
         data = [purchase_invoice.to_dict() for purchase_invoice in invoices]
         return jsonify(data), 200
 
 
-@views_bp.route('/purchase_invoices/<invoice_id>', methods=['GET'], strict_slashes=False)
+@views_bp.route('/purchase_invoices/<invoice_id>',
+                methods=['GET'], strict_slashes=False)
 def get_pur_invoice(invoice_id):
     with app.app_context():
         invoice = db.session.query(Purchase_invoice).get(invoice_id)
@@ -23,15 +26,18 @@ def get_pur_invoice(invoice_id):
         return jsonify(invoice.to_dict()), 200
 
 
-@views_bp.route('users/<user_id>/purchase_invoices', methods=['GET'], strict_slashes=False)
+@views_bp.route('users/<user_id>/purchase_invoices',
+                methods=['GET'], strict_slashes=False)
 def get_user_purchase_invoices(user_id):
     with app.app_context():
-        invoices = db.session.query(Purchase_invoice).filter_by(user_id=user_id).order_by(desc(Purchase_invoice.created_at)).all()
+        invoices = db.session.query(Purchase_invoice).filter_by(user_id=user_id)\
+            .order_by(desc(Purchase_invoice.created_at)).all()
         data = [purchase_invoice.to_dict() for purchase_invoice in invoices]
         return jsonify(data), 200
 
 
-@views_bp.route('/users/<user_id>/purchase_invoices', methods=['POST'], strict_slashes=False)
+@views_bp.route('/users/<user_id>/purchase_invoices',
+                methods=['POST'], strict_slashes=False)
 def post_pur_invoice(user_id):
     with app.app_context():
         if request.is_json:
@@ -40,11 +46,13 @@ def post_pur_invoice(user_id):
             for value in not_required:
                 if value in data:
                     abort(400, f'{value} is not required')
-            required = ['total_price', 'code', 'supplier', 'items', 'date']
+            required = ['total_price', 'code',
+                        'supplier', 'items', 'date']
             for value in required:
                 if value not in data:
                     abort(400, f'{value} is required')
-            supplier = db.session.query(Supplier).filter_by(name=data['supplier']).one()
+            supplier = db.session.query(Supplier)\
+                .filter_by(name=data['supplier']).one()
             del(data['supplier'])
             data['supplier_id'] = supplier.id       
             data['user_id'] = user_id
@@ -55,7 +63,8 @@ def post_pur_invoice(user_id):
             purchase_invoice = Purchase_invoice(**data)
             db.session.add(purchase_invoice)
             for item in items:
-                get_item = db.session.query(Item).filter_by(code=item['item']).one()
+                get_item = db.session.query(Item)\
+                    .filter_by(code=item['item']).one()
                 purchase_item_entry = purchase_item.insert().values(
                     id=str(uuid4()),
                     created_at=datetime.now(),
@@ -73,19 +82,22 @@ def post_pur_invoice(user_id):
             abort(400, 'Not a JSON')
 
 
-@views_bp.route('/purchase_invoices/<invoice_id>', methods=['DELETE'], strict_slashes=False)
+@views_bp.route('/purchase_invoices/<invoice_id>',
+                methods=['DELETE'], strict_slashes=False)
 def delete_pur_invoice(invoice_id):
     with app.app_context():
         invoice = db.session.query(Purchase_invoice).get(invoice_id)
         if invoice is None:
             abort(404)
-        db.session.execute(purchase_item.delete().where(purchase_item.c.purchase_invoice_id == invoice.id))
+        db.session.execute(purchase_item.delete()\
+                           .where(purchase_item.c.purchase_invoice_id == invoice.id))
         db.session.delete(invoice)
         db.session.commit()
         return jsonify({"message": "Invoice deleted successfully"}), 200
 
 
-@views_bp.route('/purchase_invoices/<invoice_id>', methods=['PUT'], strict_slashes=False)
+@views_bp.route('/purchase_invoices/<invoice_id>',
+                methods=['PUT'], strict_slashes=False)
 def update_pur_invoice(invoice_id):
     with app.app_context():
         invoice = db.session.query(Purchase_invoice).get(invoice_id)
@@ -93,11 +105,9 @@ def update_pur_invoice(invoice_id):
             abort(404)
         if request.is_json:
             data = request.get_json()
+            not_required = ['id', 'created_at', 'updated_at', 'user_id']
             for key, value in data.items():
-                if key == "created_at" or\
-                    key == "updated_at" or\
-                        key == 'user_id' or\
-                            key == "id":
+                if key in not_required:
                     continue
                 else:
                     setattr(invoice, key, value)
